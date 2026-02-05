8467e3b355f53f2c072f2cfde64da13292b27798
Vasu Sidapara <vasupatel303@gmail.com>
2026-02-05 16:19:28 +0530

    Display estimated_cost instead of advance_taken

--- Frontend/client/pages/common/travel/TravelApplicationDetails.tsx
+++ Frontend/client/pages/common/travel/TravelApplicationDetails.tsx
@@ -439,7 +439,7 @@ export const TravelApplicationDetails: React.FC = () => {
                               {booking.arrival_datetime}
                             </td>
                             <td className="border border-slate-200 p-3">
-                              {booking.advance_taken}
+                              {booking.estimated_cost}
                             </td>
                             <td className="border border-slate-200 p-3">
                               {booking.meal_preference || "N/A"}
@@ -680,7 +680,7 @@ export const TravelApplicationDetails: React.FC = () => {
                               {booking.check_out_datetime}
                             </td>
                             <td className="border border-slate-200 p-3">
-                              {booking.advance_taken}
+                              {booking.estimated_cost}
                             </td>
                             <td className="border border-slate-200 p-3">
                               {booking.meal_preference || "N/A"}
@@ -908,7 +908,7 @@ export const TravelApplicationDetails: React.FC = () => {
                               {booking.passengers}
                             </td>
                             <td className="border border-slate-200 p-3">
-                              {booking.advance_taken}
+                              {booking.estimated_cost}
                             </td>
                             <td className="border border-slate-200 p-3">
                               <StatusBadge
