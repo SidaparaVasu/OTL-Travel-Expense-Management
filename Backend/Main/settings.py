c19528944d770e1c33d9c14e88ab0bf45d1aa50a
Vasu Sidapara <vasupatel303@gmail.com>
2026-04-21 14:02:55 +0530

    Read EMAIL HOST and PORT from env

--- Backend/Main/settings.py
+++ Backend/Main/settings.py
@@ -328,8 +328,10 @@ SIMPLE_JWT = {
 
 # For production, use:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
+# EMAIL_PORT = 587
+EMAIL_PORT = env("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default='')
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default='')
